8777ba5f8697f00f11e560956a46e11fdfcf94c3
ashikibrahim <aashiqkibrahim@gmail.com>
2022-11-04 00:31:50 +0530

    first commit

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "../Pages/Home.css";
+import Header from "../Components/Header";
+import { Typography, Grid, Tabs, Tab } from "@mui/material";
+import CheckCircleTwoToneIcon from "@mui/icons-material/CheckCircleTwoTone";
+import SearchBar from "../Components/SearchBar";
+import { fontWeight } from "@mui/system";
+import CardComponent from "../Components/CardComponent";
+
+function Home() {
+  return (
+    <>
+      <Grid container className="full-page">
+        <Grid item className="page-top-1" xs={12} sm={8} md={8} lg={8} xl={8}>
+          <Header />
+          <div className="page-top-1-text">
+            <Typography variant="h4">
+              Easy way to find property according to your choice
+            </Typography>
+            <div className="page-top-1-text-2">
+              <Typography variant="body1">
+                We provide various property models for you at affordable prices
+                and the best quality
+              </Typography>
+            </div>
+            <SearchBar />
+            <div className="page-top-text3-main">
+              <div className="page-top-1-text-3">
+                <CheckCircleTwoToneIcon sx={{ borderColor: "" }} />
+                <Typography>100% built quality guarantee</Typography>
+              </div>
+              <div className="page-top-1-text-4">
+                <CheckCircleTwoToneIcon sx={{ backgroundColor: "" }} />
+                <Typography>More affordable price</Typography>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid
+          item
+          className="page-top-2"
+          xs={12}
+          sm={4}
+          md={4}
+          lg={4}
+          xl={4}
+        ></Grid>
+      </Grid>
+      <div className="main-2" align="center">
+        <div>
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: "", fontFamily: "fantasy", margin: "10px" }}
+          >
+            Our Property
+          </Typography>
+          <Typography sx={{ color: "#808080" }}>
+            We provide various type of properties for you with different
+            categories
+          </Typography>
+        </div>
+        <Tabs centered>
+          <Tab label="Home" sx={{ fontWeight: "Bold", fontSize: "1rem" }} />
+          <Tab label="Pricing" sx={{ fontWeight: "Bold", fontSize: "1rem" }} />
+          <Tab label="Contact" sx={{ fontWeight: "Bold", fontSize: "1rem" }} />
+        </Tabs>
+
+        <div className="card-component">
+          <CardComponent />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
